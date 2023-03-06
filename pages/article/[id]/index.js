2300232afd5779923077c203cbe00df93a1416cd
Suzana Melo <suzanamelojor@gmail.com>
2023-03-06 22:10:11 +1300

    create a page for each article

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+function ArticleId({ article }) {
+  //   const router = useRouter();
+  //   const { id } = router.query;
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href="/">Back Home</Link>
+    </>
+  );
+}
+
+// export const getServerSideProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   );
+
+//   const article = await res.json();
+
+//   return {
+//     props: {
+//       article,
+//     },
+//   };
+// };
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+  const articles = await res.json();
+
+  const ids = articles.map((article) => article.id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export default ArticleId;

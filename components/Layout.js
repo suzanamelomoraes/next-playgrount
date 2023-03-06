a5d3c284bfde16a2fe442602871a82b3d39fe152
Suzana Melo <suzanamelojor@gmail.com>
2023-03-06 19:23:00 +1300

    create a component to wrap all children in a layout container

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/Layout.module.css";
+
+function Layout({ children }) {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+}
+
+export default Layout;

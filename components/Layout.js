7a786d922184759519029ec68949ebf6a749fd5d
Suzana Melo <suzanamelojor@gmail.com>
2023-03-08 18:19:34 +1300

    apply gauce chart

--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,8 @@ import Nav from "./Nav";
 import Header from "./Header";
 import styles from "../styles/Layout.module.css";
 import Dropdown from "./Dropdown";
+import dynamic from "next/dynamic";
+const GaugeChart = dynamic(() => import("react-gauge-chart"), { ssr: false });
 
 function Layout({ children }) {
   const options = [
@@ -32,6 +34,25 @@ function Layout({ children }) {
               onChange={handleChange}
             />
           </div>
+          <div className={styles.chart}>
+            <div style={{ background: "#F6F2ED", display: "flex" }}>
+              <GaugeChart
+                nrOfLevels={420}
+                arcsLength={[0.3, 0.5, 0.2]}
+                colors={["#5BE12C", "#F5CD19", "#EA4228"]}
+                percent={0.37}
+                arcPadding={0.02}
+                style={{ width: "50%" }}
+              />
+              <GaugeChart
+                id="gauge-chart2"
+                nrOfLevels={20}
+                percent={0.86}
+                style={{ width: "50%" }}
+              />
+            </div>
+          </div>
+
           {children}
         </main>
       </div>

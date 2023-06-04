c95ef1750076f99edf5bb657a90c66453a8c7b2f
Suzana Melo <suzanamelojor@gmail.com>
2023-06-04 19:47:57 +1200

    include meta data and customise to show aboou focusing in provide a good seo

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Head from "next/head";
+// import Head from "next/head";
+import Meta from "../components/Meta";
 
 function about() {
   return (
     <div>
-      <Head>
+      {/* <Head>
         <title>About</title>
-      </Head>
+      </Head> */}
+      <Meta title="About" />
       <h1>About</h1>
     </div>
   );
